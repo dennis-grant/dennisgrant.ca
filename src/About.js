381bc7a48d791d1f3cefbda245606800fc1b81ba
Dennis Grant <dennis.grant@gmail.com>
2020-01-28 22:01:55 -0500

    update About component

--- src/About.js
+++ src/About.js
@@ -7,10 +7,23 @@ class About extends Component {
       <div className="about">
         <header>Dennis Grant</header>
         <section>
-          <p>I am a software developer with over 15 experience. My passions include learning about new technologies
-          and using structured code to solve complex user problems.</p>
+          <p>
+          I am a software developer with a passion for writing clean, structured code and a deep desire to make a difference in users' lives.
+          </p>
 
-          <p>When I am not programming I like playing soccer, writing poems and discussing philosophical ideas.</p>
+          <p>
+          With over 15 years experience, I am more prepared and feel more excited to contribute than ever before. My software development skills are at their best,
+          the lives of more people are being affected by IT in a deeper way than ever before and software development has never been more dynamic.
+          </p>
+
+          <p>
+          Please review some of my projects' source code on <a href="http://www.github.com/dennis-grant" target="_blank">Github</a> and my&nbsp; 
+          <a href="/resume">resume</a> to see some of my top skills.
+          </p>
+
+          <p>
+          When I am not programming, I like playing soccer, writing poems and discussing philosophical ideas.
+          </p>
         </section>
       </div>
     );
